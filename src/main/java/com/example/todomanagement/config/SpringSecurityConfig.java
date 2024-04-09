d6544156001078e9302672f96feb3b8fb022f9e6
Maxwell Antwi <maxwell.antwi@amalitech.com>
2024-04-09 17:37:01 +0100

    security

--- src/main/java/com/example/todomanagement/config/SpringSecurityConfig.java
+++ src/main/java/com/example/todomanagement/config/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.todomanagement.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,7 +19,9 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+    private UserDetailsService userDetailsService;
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -39,17 +44,22 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails max = User.builder()
-                .username("max")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER").build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN").build();
-
-        return new InMemoryUserDetailsManager(max, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails max = User.builder()
+//                .username("max")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER").build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(max, admin);
+//    }
 }
