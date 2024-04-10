6984cdd24021499951245d12139ced6e130dfab0
Maxwell Antwi <maxwell.antwi@amalitech.com>
2024-04-10 10:48:16 +0100

    Global exception Handler

--- src/main/java/com/example/todomanagement/exception/TodoAPIException.java
+++ src/main/java/com/example/todomanagement/exception/TodoAPIException.java
@@ -1,8 +1,10 @@
 package com.example.todomanagement.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 @AllArgsConstructor
+@Getter
 public class TodoAPIException extends RuntimeException{
     private HttpStatus status;
     private String message;
